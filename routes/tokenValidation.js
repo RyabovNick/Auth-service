2d06d3540f48c961ce0b696595542ba7298008c4
Ryabov Nikita <nikitos.aka.zed@gmail.com>
2019-04-26 16:06:49 +0300

    fix logger timestamp, add logger on success valid

--- routes/tokenValidation.js
+++ routes/tokenValidation.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const auth = require("../config/auth");
 const jwt = require("jsonwebtoken");
+const { validationLogger } = require("../lib/logger");
 
 router.route("/tokenValidation").get(auth.required, (req, res, next) => {
   let decoded = jwt.decode(req.headers.authorization.split(" ")[1]);
+  validationLogger.log("success", "successValidation", {
+    username: decoded.username
+  });
   res.send(decoded);
 });
 
