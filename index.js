dea955103380c421ca0a13c78024f355dec76a91
Ryabov Nikita <nikitos.aka.zed@gmail.com>
2019-04-29 13:49:44 +0300

    Settings to deploy on https server

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
-const errorhandler = require("errorhandler");
-const https = require("https");
-const fs = require("fs");
-const logger = require("./lib/logger");
-const helmet = require("helmet");
-const cors = require("cors");
+const errorhandler = require('errorhandler');
+const https = require('https');
+const fs = require('fs');
+const logger = require('./lib/logger');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 app.use(helmet());
 app.use(cors());
 
-const isProduction = process.env.NODE_ENV === "development";
-
-// const sslOptions = {
-//   pfx: fs.readFileSync('./sslcert.pfx'),
-//   passphrase: process.env.SSL_PASS,
-// };
+const isProduction = process.env.NODE_ENV === 'development';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -28,11 +22,11 @@ if (!isProduction) {
   app.use(errorhandler());
 }
 
-require("./config/passport");
-app.use(require("./routes"));
+require('./config/passport');
+app.use(require('./routes'));
 
 app.use((req, res, next) => {
-  let err = new Error("Not Found");
+  let err = new Error('Not Found');
   err.status = 404;
   logger.error(err);
   next(err);
@@ -47,8 +41,8 @@ if (!isProduction) {
     res.json({
       errors: {
         message: err.message,
-        error: err
-      }
+        error: err,
+      },
     });
   });
 } else {
@@ -59,20 +53,27 @@ if (!isProduction) {
     res.json({
       errors: {
         message: err.message,
-        error: {}
-      }
+        error: {},
+      },
     });
   });
 }
 
 // http
-let server = app.listen(process.env.PORT || 3000, function() {
-  console.log("Listening on port " + server.address().port);
-});
+if (process.env.NODE_ENV === 'development') {
+  let server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port ' + server.address().port);
+  });
+} else {
+  const sslOptions = {
+    pfx: fs.readFileSync('./sslcert.pfx'),
+    passphrase: process.env.SSL_PASS,
+  };
 
-// https.createServer(sslOptions, app).listen(process.env.PORT || 3000, () => {
-//     // console.log(`Listening ...`);
-// });
+  https.createServer(sslOptions, app).listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on ${process.env.PORT}`);
+  });
+}
 
 module.exports = router;
 module.exports = app;
